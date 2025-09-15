39e2ffe25c396eef5d7c6a4708482664d549ffe9
Kendra Riga <kendra@super.gd>
2025-09-15 10:08:01 -0600

    Introduce InternationalPricesClient
    While working on this I noticed a lot of duplication, so I ended up
    making a shared BasePricesClient. This also resulted in separate
    RatesSearchData classes too.

---
+++ lib/solidus_usps/base_prices_client.rb
@@ -0,0 +1,38 @@
+module SolidusUsps
+  class BasePricesClient
+    attr_reader :oauth_client, :rates_search_data
+
+    def initialize(oauth_client:, rates_search_data:)
+      @oauth_client = oauth_client
+      @rates_search_data = rates_search_data
+    end
+
+    def get_rates
+      response = connection.post(endpoint) do |request|
+        request.headers["Authorization"] = "Bearer #{@oauth_client.access_token}"
+        request.headers['Content-Type'] = 'application/json'
+        request.body = rates_search_data.to_json
+      end
+
+      handle_response(response)
+    end
+
+    private
+
+    def connection
+      Faraday.new(url: oauth_client.config.base_url) do |faraday|
+        faraday.request :json
+        faraday.response :json
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    def endpoint
+      raise NotImplementedError, 'Subclasses must implement the endpoint method'
+    end
+
+    def handle_response(_response)
+      raise NotImplementedError, "Subclasses must implement the handle_response method"
+    end
+  end
+end
--- spec/lib/solidus_usps/domestic_rates_search_data_spec.rb
+++ spec/lib/solidus_usps/domestic_rates_search_data_spec.rb
@@ -1,27 +1,30 @@
 require 'spec_helper'
 
-RSpec.describe SolidusUsps::RatesSearchData do
+RSpec.describe SolidusUsps::DomesticRatesSearchData do
   subject { described_class.new(spree_package) }
 
-  let(:stock_location) { create(:stock_location, zipcode: "12345") }
-  let(:order) { create(:order, ship_address: create(:address, zipcode: "67890")) }
+  let(:stock_location) { create(:stock_location, zipcode: '45678') }
+  let(:order) { create(:order, ship_address: create(:address, zipcode: '12345')) }
   let(:variant) { create(:variant, weight: 10.0) }
   let(:line_item) { create(:line_item, order: order, variant: variant, quantity: 1) }
-  let(:inventory_unit) { create(:inventory_unit, line_item: line_item, order: order, variant: variant) }
+  let(:inventory_unit) { create(
+    :inventory_unit,
+    line_item: line_item,
+    order: order,
+    variant: variant
+  )}
 
   let(:spree_package) do
     build(:stock_package, stock_location: stock_location).tap do |package|
-      package.add(inventory_unit)
+      package.add inventory_unit
     end
   end
 
   describe '#to_json' do
-    it "returns a JSON string with the correct structure" do
-      json_data = JSON.parse(subject.to_json)
-
-      expect(json_data).to include(
-        'originZIPCode' => "12345",
-        'destinationZIPCode' => "67890",
+    it "returns the correct JSON structure and data" do
+      expect(subject.to_json).to eq({
+        'originZIPCode' => '45678',
+        'destinationZIPCode' => '12345',
         'weight' => "10.0",
         'length' => nil,
         'width' => nil,
@@ -34,8 +37,8 @@ RSpec.describe SolidusUsps::RatesSearchData do
         'mailingDate' => nil,
         'accountType' => nil,
         'accountNumber' => nil,
-        'hasNonstandardCharacteristics' => nil
-      )
+        'hasNonstandardCharacteristics' => nil,
+      }.to_json)
     end
   end
 end
