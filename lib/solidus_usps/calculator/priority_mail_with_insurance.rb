0513e925ec09e61a4c50d312aebaea096152a90e
Kendra Riga <kendra@super.gd>
2025-09-26 10:55:10 -0600

    Port over UspsWithInsurance calculator
    From TJ - we want to ensure all the calculators they use now are
    re-implemented.

---
+++ lib/solidus_usps/calculator/priority_mail_with_insurance.rb
@@ -0,0 +1,12 @@
+module SolidusUsps
+  module Calculator
+    class PriorityMailWithInsurance < PriorityMail
+      include SolidusUsps::Calculator::Concerns::WithInsurance
+
+
+      def compute_package(package)
+        super + compute_insurance(package)
+      end
+    end
+  end
+end
