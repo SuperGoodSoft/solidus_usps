43af392e9513cc64d4282ee11539e2dd42e1d468
Adam Mueller <adam@super.gd>
2025-10-07 10:12:27 -0700

    Remove placeholder assets
    We aren't including any JS or CSS with this extension, so there's no
    need to have them required by our install generator.

--- lib/generators/solidus_usps/install/install_generator.rb
+++ lib/generators/solidus_usps/install/install_generator.rb
@@ -14,16 +14,6 @@ module SolidusUsps
         template 'initializer.rb', 'config/initializers/solidus_usps.rb'
       end
 
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_usps\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_usps\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_usps\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_usps\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
-      end
-
       def add_migrations
         run 'bin/rails railties:install:migrations FROM=solidus_usps'
       end
