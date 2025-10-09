544cd9c32db6ccc7bb086e7a74f904199475f6a2
Kendra Riga <kendra@super.gd>
2025-10-09 09:47:39 -0600

    Add PriorityMailInternational calculator
    We need it for TJ.

---
+++ spec/lib/solidus_usps/calculator/priority_mail_international_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+RSpec.describe SolidusUsps::Calculator::PriorityMailInternational do
+  subject(:calculator) { described_class.new }
+
+  let(:stock_location) { create(:stock_location) }
+  let(:variant) { create(:variant, weight: 5.0) }
+  let(:line_item) { create(:line_item, order: order, variant: variant, quantity: 1) }
+  let(:inventory_unit) {
+    create(:inventory_unit, line_item: line_item, order: order, variant: variant)
+  }
+  let(:spree_package) do
+    build(:stock_package, stock_location: stock_location).tap do |package|
+      package.add(inventory_unit)
+    end
+  end
+
+  describe "#available?" do
+    context "when shipping to the US" do
+      let(:us) { Spree::Country.find_or_create_by(iso: 'US', name: 'United States') }
+      let(:order) { create(:order, ship_address: create(:address, country: us)) }
+
+      it "returns false" do
+        expect(calculator.available?(spree_package)).to be false
+      end
+    end
+
+    context "when shipping internationally" do
+      let(:canada) { Spree::Country.find_or_create_by(iso: 'CA', name: 'Canada') }
+      let(:order) { create(:order, ship_address: create(:address, country: canada)) }
+
+      context "when package weight is 4 or less" do
+        let(:variant) { create(:variant, weight: 4.0) }
+
+        it "returns false" do
+          expect(calculator.available?(spree_package)).to be false
+        end
+      end
+
+      context "when package weight is more than 4" do
+        let(:variant) { create(:variant, weight: 5.0) }
+
+        it "returns true" do
+          expect(calculator.available?(spree_package)).to be true
+        end
+      end
+    end
+  end
+end
