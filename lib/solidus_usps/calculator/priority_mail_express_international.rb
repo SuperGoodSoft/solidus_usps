9f2843d3719ebbebf0485c2ceb6e7156e60db7dc
Kendra Riga <kendra@super.gd>
2025-10-10 07:57:06 -0600

    Add PriorityMailExpressInternational calculator

---
+++ lib/solidus_usps/calculator/priority_mail_express_international.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SolidusUsps
+  module Calculator
+    class PriorityMailExpressInternational < SolidusUsps::Calculator::Base
+      MINIMUM_WEIGHT = 4
+
+      def available? package
+        ship_to_country_code(package) != 'US' && package.weight >= MINIMUM_WEIGHT && super
+      end
+
+      def mail_class
+        "PRIORITY_MAIL_EXPRESS_INTERNATIONAL"
+      end
+
+      private
+
+      def geo_group
+        :international
+      end
+
+      def ship_to_country_code(package)
+        package.order.ship_address.country.iso
+      end
+    end
+  end
+end
---
+++ spec/lib/solidus_usps/calculator/priority_mail_express_international_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+RSpec.describe SolidusUsps::Calculator::PriorityMailExpressInternational do
+  subject(:calculator) { described_class.new }
+
+  let(:stock_location) { create(:stock_location) }
+  let(:variant) { create(:variant, weight: 2.0) }
+  let(:line_item) { create(:line_item, order: order, variant: variant, quantity: 1) }
+  let(:inventory_unit) {
+    create(:inventory_unit, line_item: line_item, order: order, variant: variant)
+  }
+  let(:spree_package) do
+    build(:stock_package, stock_location: stock_location).tap do |package|
+      package.add(inventory_unit)
+    end
+  end
+  let(:order) { create(:order, ship_address: create(:address)) }
+
+  describe "#compute_package" do
+    let(:client) { instance_double(SolidusUsps::InternationalPricesClient) }
+
+    before do
+      allow(SolidusUsps::InternationalPricesClient).to receive(:new).and_return(client)
+
+      allow(client).to receive(:get_rates).and_return("some response")
+    end
+
+    it "calls InternationalPricesClient with rates search data" do
+      calculator.compute_package(spree_package)
+      expect(SolidusUsps::InternationalPricesClient).to have_received(:new)
+
+      expect(client).to have_received(:get_rates)
+    end
+  end
+
+  describe "#available?" do
+    context "when shipping to the US" do
+      it "returns false" do
+        expect(calculator.available?(spree_package)).to be false
+      end
+    end
+
+    context "when shipping internationally" do
+      let(:order) { create(:order, ship_address: create(:address, country: create(:country, iso: 'CA'))) }
+
+      context "when the package weight is below 4 lbs" do
+        let(:variant) { create(:variant, weight: 3.0) }
+
+        it "returns false" do
+          expect(calculator.available?(spree_package)).to be false
+        end
+      end
+
+      context "when the package weight is 4 lbs or above" do
+        let(:variant) { create(:variant, weight: 5.0) }
+
+        it "returns true" do
+          expect(calculator.available?(spree_package)).to be true
+        end
+      end
+    end
+  end
+end
